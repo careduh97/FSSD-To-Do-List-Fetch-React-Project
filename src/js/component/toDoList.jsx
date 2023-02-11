0577cce289c8d2fb18a63d6760530e494b5b0ac9
Carlos Herrera <c.herrerav97@gmail.com>
2023-02-11 16:42:40 +0000

    Ajustes varios

--- src/js/component/toDoList.jsx
+++ src/js/component/toDoList.jsx
@@ -1,16 +1,115 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
 export function ListApp () {
     const [inputValue, setInputValue] = useState("");
     const [items, setItems] = useState([]);
-    const enterKey = function (event) {
-        return event.code === "Enter" ? (setItems([...items, inputValue]), setInputValue("")) : "";
+    const [buttonActive, setButtonActive] = useState(true)
+    
+    const enterKey = async (event) => {
+        const newTask = { label: inputValue, done: false };
+        if (event.code !== "Enter") {
+            return;
+        }
+        if (inputValue.trim() !== "") {
+            try {
+                const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/careduh97", {
+                    method: "PUT",
+                    body: JSON.stringify([...items, newTask]),
+                    headers: {
+                        "Content-Type": "application/json",
+                    }
+                })
+                if (response.status == 200) {
+                    getTasks();
+                    setInputValue("");
+                }
+                else {
+                    alert("Tu tarea no se guardo satisfactoriamente");
+                    throw new Error (response.status);
+                }
+            } catch (error) {
+                console.log("Estatus de error: ", error);
+            }
+        }
     };
 
-    const deleteKey = function (indexTarget) {
-        return setItems(items.filter((element, indexFilter) => indexFilter !== indexTarget))
+    const deleteKey = async (indexTarget) => {
+        let deleteTask = items.filter((element, indexFilter) => indexFilter !== indexTarget);
+        if (deleteTask.length == 0){
+            deleteTask = [{ label: "Sample Task", done: false }];
+        }
+        try {
+            const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/careduh97", {
+                method: "PUT",
+                body: JSON.stringify(deleteTask),
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            })
+            if (response.status == 200) {
+                getTasks();
+            }
+            else {
+                alert("Tu tarea no se pudo eliminar satisfactoriamente");
+                throw new Error (response.status);
+            }
+        } catch(error) {
+            console.log("Estatus de error: ", error);
+        }
     };
+
+    const deleteUser = async () => {
+        try {
+            const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/careduh97", {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            })
+            if (response.status !== 200) {
+                alert("Tu tarea no se pudo eliminar satisfactoriamente");
+                throw new Error (response.status);
+            }
+            const userCreated = await createUser();
+            if (userCreated == true) {
+                await getTasks();
+            }
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    const createUser = async () => {
+        try{
+            const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/careduh97", {
+                method: "POST",
+                body: JSON.stringify([]),
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            })
+            return response.status == 200
+        } catch(error){
+            console.log(error);
+        }
+    }
+
+    const getTasks = async () => {
+        try {
+            const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/careduh97");
+            const body = await response.json();
+            console.log(body);
+            setItems(body);
+            setButtonActive(true);
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getTasks();
+    }, [])
 
     return (
         <div className="listBody">
@@ -22,24 +121,41 @@ export function ListApp () {
                 placeholder="What needs to be done?">
             </input>
 
-            {items.length == 0 ? 
+            {items.length == 1 ? 
                 <ul>
                     No tasks yet. Please add some!
                 </ul> 
                 :
                 <ul>
                     {items.map(function(element, indexMap) {
-                        return (
-                            <li key={indexMap}>
-                                <span>{element}</span> 
-                                <i onClick={event => {deleteKey(indexMap)}} className="fa-solid fa-xmark"></i>
-                            </li>
-                        )       
+                        if (indexMap > 0) {
+                            return (
+                                <li key={indexMap}>
+                                    <span>{element.label}</span> 
+                                    <i onClick={event => {deleteKey(indexMap)}} className="fa-solid fa-xmark"></i>
+                                </li>
+                            )       
+                        }
                     })}
                 </ul>
             }
+            
+            {
+                items.length > 0 ? (
+                    <p className="footerCounter mt-2">{items.length - 1} tasks added</p>
+                ) : ""
+            }
 
-            <p className="footerCounter mt-2">{items.length} tasks added</p>
+            <button type="button" 
+                onClick={() => {
+                    deleteUser()
+                    setButtonActive(false)
+                }} 
+                className="btn btn-light mt-2" 
+                disabled={buttonActive ? "" : "disabled" }
+            >
+                Delete All Tasks
+            </button>
 
             {console.log(inputValue)}
             {console.log(items)}
